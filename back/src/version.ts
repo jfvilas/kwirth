5b1f73bb7492d52f28daea0de9677a8adb49df7c
Julio <jfvilas@outlook.com>
2026-03-17 19:45:13 +0100

    bew source for electron

--- back/src/version.ts
+++ back/src/version.ts
@@ -1,1 +1,1 @@
-export const VERSION:string="0.4.365"; 
+export const VERSION:string="0.4.366"; 
--- electron/version.js
+++ electron/version.js
@@ -1,1 +1,1 @@
-const VERSION="0.4.365"; module.exports = { VERSION }; 
+const VERSION="0.4.366"; module.exports = { VERSION }; 
